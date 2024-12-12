b7b143e7968610d02a1c0ab929b697d4124a0cc6
KorMH <105302873+KorMH@users.noreply.github.com>
2024-12-13 03:01:11 +0900

    Delete cms/user-api/src/main/java/com/zerobase/cms/user/client directory
    API KEY 삭제

--- cms/user-api/src/main/java/com/zerobase/cms/user/client/domain/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zerobase.cms.user.client.domain.repository;
-
-import com.zerobase.cms.user.client.domain.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
